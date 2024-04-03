59772d082ab5b23512d4c53464bbcc25ea9793bf
Luis Eduardo Leme Silva <Luis_leme09@hotmail.com>
2024-04-03 20:47:10 -0300

    Páginas

---
+++ script.js
@@ -0,0 +1,13 @@
+function troca(){
+    
+    /*
+    if (html.classList.constains('light')){
+        html.classList.remove('light')
+     } else {
+        html.classList.add('light')
+     }
+     */
+
+    const html = document.documentElement
+    html.classList.toggle ('light')
+}
